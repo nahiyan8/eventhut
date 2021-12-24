5201f4e9fcc31550d8b59459e64c8307348a34f8
Zulker Nayeen Nahiyan <nahiyan8@users.noreply.github.com>
2021-12-24 13:19:35 +0600

    Add page data objects

--- views/event.php
+++ views/event.php
@@ -1,10 +1,21 @@
+<?php
+
+$event = [
+  'id' => 1234,
+  'type' => 'Lecture',
+  'title' => 'The Joys and Stresses of the Holiday Season – A Workshop on Self-Compassion',
+  'date' => 'Tuesday, December 14, 2021',
+  'time' => '12:00 pm – 1:30 pm',
+]
+
+?>
 <!doctype html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>TEMPLATE - EventHut</title>
+  <title>Event - EventHut</title>
   
   <link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
@@ -13,8 +24,8 @@
   <!-- Navbar -->
   <?php include '../components/navbar.php'; ?>
   <!-- Main -->
-  <main class="container col-12 col-md-10 offset-md-2 bg-body border">
-    
+  <main class="container col-12 col-md-8 offset-md-2 bg-body border px-4 py-2">
+    <h1><?= event['title']; ?></h1>
   </main>
 </body>
 
