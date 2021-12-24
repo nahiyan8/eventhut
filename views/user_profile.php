4781a87cac07cf72c0a28a03e09cedfe887fb684
sNaziaHaque <nazia.haque.snh@gmail.com>
2021-12-24 13:30:37 +0600

    updated user profile

--- views/user_profile.php
+++ views/user_profile.php
@@ -13,14 +13,14 @@
   <!-- Navbar -->
   <?php include '../components/navbar.php'; ?>
   <!-- Main -->
-  <main class="container col-12 col-md-10 offset-md-2">
+  <main class="container col-12 col-md-8 offset-md-2 bg-body border px-4 py-2">
   <section class="section about-section gray-bg" id="about">
             <div class="container">
                 <div class="row align-items-center flex-row-reverse">
                     <div class="col-lg-6">
                         <div class="about-text go-to">
-                            <h3 class="dark-color">User Name</h3>
-                            <h6 class="theme-color lead">Bio</h6>
+                            <h3 class="dark-color"><?= $event['User Name']; ?></h3>
+                            <h6 class="theme-color lead"><?= $event['Bio']; ?></h6>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
                             <div class="row about-list">
                                 <div class="col-md-6">
@@ -64,19 +64,19 @@
                     <div class="row">
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="500" data-speed="500">500</h6>
+                                <h6 class="count h2" data-to="500" data-speed="500"><?= $event['500']; ?></h6>
                                 <p class="m-0px font-w-600">Upcoming Events</p>
                             </div>
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="150" data-speed="150">150</h6>
+                                <h6 class="count h2" data-to="150" data-speed="150"><?= $event['150']; ?></h6>
                                 <p class="m-0px font-w-600">Registered Events</p>
                             </div>
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="850" data-speed="850">850</h6>
+                                <h6 class="count h2" data-to="850" data-speed="850"><?= $event['200']; ?></h6>
                                 <p class="m-0px font-w-600">Participated Events</p>
                             </div>
                         </div>
