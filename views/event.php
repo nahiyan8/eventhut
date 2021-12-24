8597bc987141994f093da9ccddc3ad82e9a941bf
Zulker Nayeen Nahiyan <nahiyan8@users.noreply.github.com>
2021-12-24 11:49:08 +0600

    Add templates for all needed pages

---
+++ views/event.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TEMPLATE - EventHut</title>
+  
+  <link rel="stylesheet" type="text/css" href="/css/main.css">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <?php include '../components/navbar.php'; ?>
+  <!-- Main -->
+  <main class="container col-12 col-md-10 offset-md-2">
+  </main>
+  <!-- JavaScript -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</body>
+
+</html>
