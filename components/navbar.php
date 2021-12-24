7529fbbe90325deb06524a62fa1662af90d3f717
Zulker Nayeen Nahiyan <nahiyan8@users.noreply.github.com>
2021-12-24 13:01:51 +0600

    Navbar changes (temporary)

--- components/navbar.php
+++ components/navbar.php
@@ -1,19 +1,39 @@
+<?php
+
+$pages = [
+    '/views/home.php' => 'Home',
+    '/views/event.php' => 'Event',
+    '/views/login.php' => 'Login',
+    '/views/register.php' => 'Register',
+    '/views/user_profile.php' => 'User Profile',
+    '/views/event_create.php' => 'Event Create',
+    '/views/event_listing.php' => 'Event Listing',
+    '/views/event_manage.php' => 'Event Manage',
+    '/views/event_register.php' => 'Event Register',
+    '/views/event_attendees.php' => 'Event Attendees'
+];
+
+?>
 <!-- Navbar -->
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">EventHut</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Create Event</a>
-            </li>
+            <?php
+                foreach ($pages as $uri => $name) {
+                    $active = $uri == $_SERVER['REQUEST_URI'] ? ' active' : '';
+                    echo '<li class="nav-item">';
+                    echo '<a class="nav-link' . $active . '" aria-current="page" href="' . $uri . '">';
+                    echo $name;
+                    echo '</a>';
+                    echo '</li>';
+                }
+            ?>
         </ul>
         <button class="btn btn-outline-success" type="submit">Hello, {name}</button>
         </div>
